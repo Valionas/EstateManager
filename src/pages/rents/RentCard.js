447e884cbe11579d55fe23d76ca6e4dbc5609e7b
Valionas <sdms@abv.bg>
2022-07-10 18:07:04 +0300

    RentCard Improvement

--- src/pages/rents/RentCard.js
+++ src/pages/rents/RentCard.js
@@ -1,36 +1,35 @@
 import { Col, Row, Image } from 'antd';
-import { Space, Table, Tag } from 'antd';
+import { Space, Table, Tag, Button } from 'antd';
+import { GoLocation } from 'react-icons/go'
+import './Rents.css';
 
 function RentCard({ rentObject }) {
     return (
-        <>
+        <div className='rentCard'>
             <Row>
-                <Col span={3}>
+                <Col span={4} style={{ display: 'flex', justifyContent: "center", alignItems: 'center' }}>
                     <Image
+                        height={"100%"}
                         src={rentObject.imageUrl}
                     />
                 </Col>
-                <Col span={12} offset={1}>
+                <Col span={13} offset={1}>
                     <h1>{rentObject.name}</h1>
+                    <p><GoLocation /> {rentObject.location}</p>
+                    <h1>Description</h1>
                     <p>{rentObject.description}</p>
                 </Col>
-                <Col span={6} offset={2}>
+                <Col span={4} offset={1}>
                     <Row>
-                        <p style={{ fontSize: 26 }}>$ {rentObject.rent}/monthly</p>
+                        <p style={{ fontSize: "3vh" }}><b>Price:</b> ${rentObject.rent}/monthly</p>
                     </Row>
                     <Row>
-                        <Col span={6}>
-                            <Image
-                                src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-                            />
-                        </Col>
-                        <Col span={16} offset={2}>
-                            <p>{rentObject.owner}</p>
-                        </Col>
+                        <Button type="primary" shape="round" style={{ width: "100%", marginBottom: '5%' }}>REQUEST RENT</Button>
+                        <Button type="primary" shape="round" style={{ width: "100%" }}>REVIEW</Button>
                     </Row>
                 </Col>
             </Row>
-        </>
+        </div>
     )
 }
 
