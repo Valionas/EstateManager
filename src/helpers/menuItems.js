fb2a25731345ec8cb4872296ced9025f4c5c6c65
Valionas <38500314+Valionas@users.noreply.github.com>
2022-07-24 13:09:07 +0300

    Update Menu Items

--- src/helpers/menuItems.js
+++ src/helpers/menuItems.js
@@ -12,7 +12,7 @@ import { AiOutlineUserAdd, AiOutlineUserSwitch } from 'react-icons/ai';
 import { MdOutlinePayment, MdOutlineSell, MdCreate } from 'react-icons/md'
 import { HiOutlineDocumentReport } from 'react-icons/hi';
 import { BiLogOut } from 'react-icons/bi';
-import { GiMoneyStack } from 'react-icons/gi';
+import { GiMoneyStack, GiModernCity } from 'react-icons/gi';
 
 function setMenuItem(label, key, icon, children) {
     return {
@@ -28,14 +28,17 @@ export const publicMenuItems = [
     setMenuItem(<Link to="/login">Login</Link>, '2', <AiOutlineUserSwitch />),
     setMenuItem(<Link to="/register">Register</Link>, '3', <AiOutlineUserAdd />),
     setMenuItem(<Link to="/rents">Rents</Link>, '4', <MdOutlinePayment />),
+    setMenuItem(<Link to="/estates">Estates for sale</Link>, '5', <GiModernCity />),
+
 ];
 
 export const authenticatedMenuItems = [
     setMenuItem(<Link to="/">Home</Link>, '1', <PieChartOutlined />),
     setMenuItem(<Link to="/rents">Rents</Link>, '2', <MdOutlinePayment />),
-    setMenuItem(<Link to="/sales">Sales</Link>, '3', <MdOutlineSell />),
-    setMenuItem(<Link to="/rent-requests">Rent Requests</Link>, '4', <GiMoneyStack />),
-    setMenuItem(<Link to="/reports">Reports</Link>, '5', <HiOutlineDocumentReport />),
-    setMenuItem(<Link to="/logout">Logout</Link>, '6', <BiLogOut />),
+    setMenuItem(<Link to="/rent-requests">Rent Requests</Link>, '3', <GiMoneyStack />),
+    setMenuItem(<Link to="/estates">Estates for sale</Link>, '4', <GiModernCity />),
+    setMenuItem(<Link to="/sales">Estate Sales</Link>, '5', <MdOutlineSell />),
+    setMenuItem(<Link to="/reports">Reports</Link>, '6', <HiOutlineDocumentReport />),
+    setMenuItem(<Link to="/logout">Logout</Link>, '7', <BiLogOut />),
 ];
 
